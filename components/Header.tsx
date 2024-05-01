72ad9a9ca09b37d8c7615448b6c13a5a203e6340
Giuseppe Funicello <g.funicello@gmail.com>
2024-05-01 15:14:59 +0900

    feat: implements dictionary and moving all under [lang] wrapper folder

---
+++ components/Header.tsx
@@ -0,0 +1,13 @@
+import LangSwitcher from "@/components/LangSwitcher";
+
+const Header = () => {
+    return (
+        <header className="py-6">
+            <div>
+                <LangSwitcher />
+            </div>
+        </header>
+    );
+};
+
+export default Header;
--- middleware.ts
+++ middleware.ts
@@ -35,10 +35,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-    matcher: [
-        // Skip all internal paths (_next)
-        "/((?!_next).*)",
-        // Optional: only run on root (/) URL
-        // '/'
-    ],
+    matcher: ["/((?!api|_next/static|_next/image|favicon.ico|next.svg).*)"],
 };
