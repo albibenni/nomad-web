1c46c4412ac497b562a3dfda4d7d93ee8d86ee0f
Giuseppe Funicello <g.funicello@gmail.com>
2024-05-01 15:25:33 +0900

    feat:orm Drizzle

---
+++ lib/migrate.ts
@@ -0,0 +1,15 @@
+import { drizzle } from "drizzle-orm/postgres-js";
+import { migrate } from "drizzle-orm/postgres-js/migrator";
+import postgres from "postgres";
+import * as dotenv from "dotenv";
+
+dotenv.config({ path: "@/.env" });
+console.log("migrate");
+const connectionString = process.env.POSTGRES_URL + "";
+const sql = postgres(connectionString, { max: 1 });
+export const db = drizzle(sql);
+
+await migrate(db, { migrationsFolder: "@/drizzle" });
+
+await sql.end();
+console.log("end migrate");
